9dd9483372e917ebc808a6c62da3954cea1e4fcf
Redox <mail@redox.to>
2022-11-15 18:14:32 +0100

    Remove features from vjb tool menu cat

--- lua/autorun/client/cl_vjb_junk_remover.lua
+++ lua/autorun/client/cl_vjb_junk_remover.lua
@@ -1,1 +1,6 @@
 VJF = true
+
+hook.Add( "AddGamemodeToolMenuTabs", "RemoveVJBJunkTools", function()
+    hook.Remove( "AddToolMenuTabs", "VJ_CREATETOOLTAB" )
+    hook.Remove( "PopulateToolMenu", "VJ_ADDTOMENU_WEAPON" )
+end )
